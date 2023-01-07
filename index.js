abf32b535c0340e125a6f5e29ea1f6d7bad0b01b
catzilla <catzillaorz@outlook.com>
2023-01-07 14:33:35 +0800

    feat: core api | readFileSync(path, encode)

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // run `node index.js` in the terminal
+const fs = require('fs')
+const text = fs.readFileSync('./txt/input.txt', 'utf8')
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(text)
+
+
